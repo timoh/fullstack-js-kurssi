a16d5dfba6038102da13f65ba366e7591712ef86
Timo H <timo.herttua@efekta.fi>
2018-01-28 14:29:42 +0200

    osa24

--- osa2/osa2-kurssit/src/index.js
+++ osa2/osa2-kurssit/src/index.js
@@ -2,23 +2,45 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 const App = () => {
-  const kurssi = {
-    nimi: 'Half Stack -sovelluskehitys',
-    osat: [
-      {
-        nimi: 'Reactin perusteet',
-        tehtavia: 10
-      },
-      {
-        nimi: 'Tiedonvälitys propseilla',
-        tehtavia: 7
-      },
-      {
-        nimi: 'Komponenttien tila',
-        tehtavia: 14
-      }
-    ]
-  }
+  const kurssit = [
+    {
+      nimi: 'Half Stack -sovelluskehitys',
+      id: 1,
+      osat: [
+        {
+          nimi: 'Reactin perusteet',
+          tehtavia: 10,
+          id: 1
+        },
+        {
+          nimi: 'Tiedonvälitys propseilla',
+          tehtavia: 7,
+          id: 2
+        },
+        {
+          nimi: 'Komponenttien tila',
+          tehtavia: 14,
+          id: 3
+        }
+      ]
+    },
+    {
+      nimi: 'Node.js',
+      id: 2,
+      osat: [
+        {
+          nimi: 'Routing',
+          tehtavia: 3,
+          id: 1
+        },
+        {
+          nimi: 'Middlewaret',
+          tehtavia: 7,
+          id: 2
+        }
+      ]
+    }
+  ]
 
   const Kurssi = (props) => {
     return(
@@ -67,7 +89,9 @@ const App = () => {
 
   return (
     <div>
-      <Kurssi kurssi={kurssi} />
+      <h1>Opetusohjelma</h1>
+      {kurssit.map(kurssi => <Kurssi kurssi={kurssi} />)}
+      
     </div>
   )
 }
